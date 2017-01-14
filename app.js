9b4e4f37a4a9bd02d6351491bc78a9074d0af6e8
cmonte905 <cmonte905@hotmail.com>
2017-01-14 20:57:38 +0000

    second commit

---
+++ app.js
@@ -0,0 +1,41 @@
+var tmi = require('tmi.js');
+
+var options = {
+	options: {
+		debug: true
+	},
+	connection: {
+		cluster: "aws",
+		reconnect : true
+	},
+	identity: {
+		username: "lenny_face_bot",
+		password: "oauth:na2hyb9rhoh6xhxon8wd8sr9sr2ys9"	
+	},
+	channels: ["stanseven"]
+
+};
+
+var client = new tmi.client(options);
+client.connect();
+
+client.on('connected', function(address, port){
+	client.action("stanseven", "( ͡° ͜ʖ ͡°) you have been struck by a smooth criminal ( ͡° ͜ʖ ͡°)\n" +
+	" ( ͡° ͜ʖ ͡°) \n type !help for more options ( ͡° ͜ʖ ͡°) ");
+});
+
+client.on('chat', function(channel, user, message, self){
+	
+	if(message === "!twitter")	 {
+		client.action("stanseven", "no twitter for you you twit\n( ͡° ͜ʖ ͡°) ( ͡° ͜ʖ ͡°) ( ͡° ͜ʖ ͡°) ( ͡° ͜ʖ ͡°) ( ͡° ͜ʖ ͡°) ( ͡° ͜ʖ ͡°) ( ͡° ͜ʖ ͡°) ( ͡° ͜ʖ ͡°) ( ͡° ͜ʖ ͡°) ( ͡° ͜ʖ ͡°) ( ͡° ͜ʖ ͡°) ( ͡° ͜ʖ ͡°) ( ͡° ͜ʖ ͡°) ( ͡° ͜ʖ ͡°) ( ͡° ͜ʖ ͡°) ( ͡° ͜ʖ ͡°) ( ͡° ͜ʖ ͡°) ( ͡° ͜ʖ ͡°) ( ͡° ͜ʖ ͡°) ( ͡° ͜ʖ ͡°) ( ͡° ͜ʖ ͡°)  ( ͡° ͜ʖ ͡°) ");
+	}
+	else if(message === "!help"){
+		console.log(user +" " +  message);
+		client.action("stanseven", "there is !twitter and thats about it ( ͡° ͜ʖ ͡°)( ͡° ͜ʖ ͡°) ");
+	}	
+	
+	else if(message.includes('!parse')) { 
+		client.action("stanseven");
+	}
+
+});
